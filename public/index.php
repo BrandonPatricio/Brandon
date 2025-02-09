a57d9fc1ad7b88cf28c951ad59ae449fc483065b
Brandon Patricio <brandonoswaldo77@gmail.com>
2025-02-08 22:10:58 -0600

    Primera versión: index y database con

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+require '../config/google-config.php';
+$login_url = $client->createAuthUrl();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Iniciar Sesión</h2>
+    <a href="<?= $login_url ?>">Iniciar con Google</a>
+</body>
+</html>
